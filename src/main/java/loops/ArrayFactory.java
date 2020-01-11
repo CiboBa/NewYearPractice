2c422b7097df77b53c42734bda7ef038c70f934d
Bartek Cib <bciborowski.pl@gmail.com>
2020-01-11 11:33:39 +0100

    Task 8 solution added.

--- src/main/java/loops/ArrayFactory.java
+++ src/main/java/loops/ArrayFactory.java
@@ -1,19 +1,58 @@
 package loops;
 
+import java.util.Arrays;
+
 //3. Napisz klasę ArrayFactory, która w konstruktorze pobierze liczbę całkowitą X. Klasa powinna mieć 2 metody:
 public class ArrayFactory {
 
-    public ArrayFactory arrayFactory(int i) {
-        return new ArrayFactory();
+    public static void main(String[] args) {
+        int i = 3;
+        ArrayFactory arrayFactory = new ArrayFactory(i);
+        int[] oneDimensionArray = oneDimension(i);
+        printOneDimensionArray(oneDimensionArray);
+        int[][] twoDimensionArray = twoDimension(i);
+        printTwoDimensionArray(twoDimensionArray);
+        int[][] filledArray= fillUnitMatrix(twoDimensionArray);
+        printTwoDimensionArray(filledArray);
+    }
+
+
+    public ArrayFactory(int i) {
     }
 
     //3a. int[] oneDimension – zwróci instancję tablicy jednowymiarowej o długości X,
-    public int[] oneDimension(int i) {
+    public static int[] oneDimension(int i) {
         return new int[i];
     }
 
     //3b. int[][] twoDimension – zwróci instację tablicy dwuwymiarowej gdzie liczba wierszy i liczba kolumn równa się X,
-    public int[][] twoDimension(int i){
+    public static int[][] twoDimension(int i) {
         return new int[i][i];
+    }
+
+    private static void printOneDimensionArray(int[] oneDimensionArray) {
+        System.out.println(Arrays.toString(oneDimensionArray));
+    }
+
+    private static void printTwoDimensionArray(int[][] twoDimensionArray) {
+        for (int j = 0; j < twoDimensionArray.length; j++) {
+            for (int k = 0; k < twoDimensionArray[j].length; k++) {
+                System.out.print(twoDimensionArray[j][k]);
+            }
+            System.out.println();
+        }
+        System.out.println("----------");
+    }
+
+    //8. Do klasy ArrayFactory dodaj metodę zwracającą macierz jednostkową (jedynki “na przekątnej”),
+    public static int[][] fillUnitMatrix(int[][] twoDimension) {
+        for (int i = 0; i < twoDimension.length; i++) {
+            for (int j = 0; j < twoDimension[i].length; j++) {
+                if (i == j) {
+                    twoDimension[i][j] = 1;
+                } else twoDimension[i][j] = 0;
+            }
+        }
+        return twoDimension;
     }
 }
--- src/main/java/loops/Loops.java
+++ src/main/java/loops/Loops.java
@@ -3,12 +3,47 @@ package loops;
 public class Loops {
 
     public static void main(String[] args) {
-        int a = 134;
+        int a = 123;
         showDigitsToX(a);
         showDigitsToY(a);
         printReversedNumber(a);
+
+        String word = "pies";
+        reverseWord(word);
+
+        Integer b = 5;
+        binaryForm(b);
+
+        String wordToCheck = "kajak";
+        System.out.println(isPalindrom(wordToCheck));
     }
 
+    // 7. Utwórz metodę, pobierającą łańcuch znaków, która sprawdzi czy jest on palindromem.
+    // Np. “kajak” jest palindromem (to samo czytane “od przodu i od tyłu”) jednak “kotek” już nie. Może przydać Ci się metoda String#toCharArray
+    private static boolean isPalindrom(String wordToCheck) {
+        StringBuffer stringBuffer=new StringBuffer(wordToCheck);
+        stringBuffer.reverse();
+        if (String.valueOf(stringBuffer).equalsIgnoreCase(wordToCheck)){
+            return true;
+        }
+        return false;
+    }
+
+    //6. Utwórz metodę, która pobierze liczbę oraz zwróci ją w formie binarnej (2 => “10”, 4 => “100”, 5 => “101”, itd.). System binarny opisałem w osobnym artykule,
+    private static void binaryForm(Integer b) {
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println("---------------");
+    }
+
+    //5. Utwórz metodę, która jako argument pobierze obiekt klasy String i zwróci “odwrócony” argument. Na przykład “pies” przekształci w “seip”,
+    private static void reverseWord(String word) {
+        StringBuffer stringBuffer = new StringBuffer(word);
+        stringBuffer.reverse();
+        System.out.println(stringBuffer);
+        System.out.println("---------------");
+    }
+
+    //4. Utwórz metodę, która pobierze liczbę i wypisze każdy znak w osobnej linii zaczynając od ostatniej cyfry (np. dla liczby 123 będą to trzy linie z 3, 2 i 1),
     private static void printReversedNumber(int z) {
         String zValueString = String.valueOf(z);
         char[] zValueArray = new char[zValueString.length()];
